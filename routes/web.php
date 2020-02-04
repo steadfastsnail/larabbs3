dd5574b02f667c54d12847b670fdf4605468c3c5
steadfastsnail <steadfast2015@163.com>
2020-02-04 01:50:14 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::resource('users', 'UsersController', ['only'=>['show', 'edit', 'update']]
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only'=>['show']]);
+
+//图片上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
