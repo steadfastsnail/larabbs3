f1735420c4e1d51da239ee1665773bbea5c25afe
steadfastsnail <steadfast2015@163.com>
2020-02-09 07:15:38 +0000

    Admin can manage roles and permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -65,9 +65,8 @@ class User extends Authenticatable
 
     public function setPasswordAttribute($value)
     {
-        // 如果值的长度等于 60，即认为是已经做过加密的情况
-        if (strlen($value) != 60) {
-
+        $c= strlen($value);
+        if($c && $c!=60){
             // 不等于 60，做密码加密处理
             $value = bcrypt($value);
         }
